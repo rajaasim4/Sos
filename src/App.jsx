39ab08fe82cfc640a55cc4a4046fea8b2ef53237
rajaasim4 <rajaasim4474@gmail.com>
2023-04-30 20:12:55 +0500

    Vercel Analytics Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home/Home";
+
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   return (
     <>
       <Home />
+
+      <Analytics />
     </>
   );
 };
